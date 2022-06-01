a6395dfbbb7e61410b905d3299b5b5e668d17110
Stefan Markov <stefan.t.markov@gmail.com>
2022-06-01 16:29:58 +0300

    Add comments to recipe details.

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import { loginPage } from './views/login.js';
 import { registerPage } from './views/register.js';
 import { catalogPage } from './views/catalog.js';
 import { createPage } from './views/create.js';
+import { detailsPage } from './views/details.js';
 
 page(addSession());
 page(decorateContext());
@@ -17,5 +18,6 @@ page('/login', loginPage);
 page('/register', registerPage);
 page('/catalog', catalogPage);
 page('/create', createPage);
+page('/details/:id', detailsPage);
 
 page.start();
